ef3bee613ed718b556c19fad27f634340629cf57
robstallion <robertefrancis18@gmail.com>
2017-06-16 12:27:29 +0100

    calls function to run node script and prints the response from the db in the terminal.

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule ElixirNodeMssql.PageController do
   use ElixirNodeMssql.Web, :controller
 
   def index(conn, _params) do
+    run_node_script(["tedious.js", "SELECT pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid"])
+    |> IO.inspect(label: "=====> response")
     render conn, "index.html"
   end
 
